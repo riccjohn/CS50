66479578698c9e60983aac9d834e307a93f2a8cf
John Riccardi <riccjohn@gmail.com>
2015-04-07 22:51:10 -0400

    Deleted extra files and changed caesar.c to use arguments instead of asking for a key

--- Week_02/pset2/caesar.c
+++ Week_02/pset2/caesar.c
@@ -2,12 +2,16 @@
 #include <string.h>
 #include <cs50.h>
 
-int main(int argc, string argv[])
+int main(int argc, char* argv[])
+//int main(void)
 {
   if (argc == 2)
   {
-    printf("Encryption Key: ");
-    int key = GetInt();
+    // printf("Encryption Key: ");
+    // int key = GetInt();
+    string k = argv[1];
+    // convert argv string to an int
+    int key = atoi(k);
     if (key >= 0 && key <= 26)
     {
       printf("Key set to %i\n", key);
@@ -28,13 +32,14 @@ int main(int argc, string argv[])
     }
     printf("\n");
     printf("Input message to be encrypted:\n");
-    string s = GetString();
+    char* s = GetString();
     printf("\n");
     printf("Encrypted Message:\n");
 
     // Add key value to original plaintext char value
     // Still need to figure out how to get characters to not go past 'z' or 'Z'j
     // Maybe another 'if' resulting value is <z || <Z, - 26
+
     for (int i = 0, n = strlen(s); i < n; i++)
     {
       if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
